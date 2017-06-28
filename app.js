e06c5d67606c280fded528e9063d2bde56a078be
Delink-D <delinkdesigns@gmail.com>
2017-06-28 21:19:12 +0300

    Inporting all required files/dependancies
    config file that holds credentials to db
    nodemailer to facilitate emails notification

---
+++ app.js
@@ -0,0 +1,5 @@
+// require the needed files and depedancies
+// config file that holds the couch credentials
+// nodemailer for facilitating the sending of emails
+var config = require("./config/config");
+var nodemailer = require('nodemailer');
