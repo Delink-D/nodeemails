bc95791a00ddda842f22ee5acf793003391d0886
Delink-D <delinkdesigns@gmail.com>
2017-06-28 21:22:21 +0300

    Setting up the transport object for email sending
    Transport object is a reusable object, using the default SMTP transport

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 // nodemailer for facilitating the sending of emails
 var config = require("./config/config");
 var nodemailer = require('nodemailer');
+
+'use strict';
+// // create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+        user: config.email.emailid,
+        pass: config.email.password
+    }
+});
